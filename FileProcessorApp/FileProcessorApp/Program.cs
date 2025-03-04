24bcd3adce0de9c36a99a2294689580b0f691d69
Surendra Behara <97676024+surendrabehara2022@users.noreply.github.com>
2025-03-04 18:49:40 +0530

    Add files via upload

---
+++ FileProcessorApp/FileProcessorApp/Program.cs
@@ -0,0 +1,24 @@
+﻿class Program
+{
+    public static async Task Main(string[] args)
+    {
+        Console.WriteLine("Starting dummy background processing...");
+        var processingStartTime = DateTime.Now;
+
+        for (var i = 0; i < 88; i++)
+        {
+            await ProcessFileAsync(i);
+        }
+
+        Console.WriteLine("Dummy background processing completed.");
+        Console.WriteLine("Total time to complete the processing in seconds: " + (DateTime.Now - processingStartTime).TotalSeconds);
+    }
+
+    public static async Task ProcessFileAsync(int number)
+    {
+        // Simulate file processing
+        Console.WriteLine($"Processing start for: {number}");
+        await Task.Delay(20000); // Simulate some delay
+        Console.WriteLine($"Completed processing for: {number}");
+    }
+}
