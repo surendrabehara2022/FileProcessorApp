ce23c70cb0cc518ec495e751143f864ae44dddbc
Surendra Behara <97676024+surendrabehara2022@users.noreply.github.com>
2025-03-07 15:04:26 +0530

    Update azure.tf

--- containerapp_job/azure.tf
+++ containerapp_job/azure.tf
@@ -9,16 +9,17 @@ terraform {
  # required_version = "=1.3.2"
   required_version = ">= 1.11.0"
   backend "azurerm" {
+    resource_group_name  = "tfstatesa"
+    storage_account_name = "tf0001sa"
     container_name = "tfstate"
     key            = "terraform.deployment.tfplan"
+    use_oidc             = true
+    subscription_id      = "81e63c6d-7f3c-4e33-8a28-0036da979a43"
+    tenant_id            = "add1c500-a6d7-4dbd-b890-7f8cb6f7d861"
   }
 }
 
-#provider "azurerm" {
-#  features {}
-#}
-
 provider "azurerm" {
+  use_oidc = true
   features {}
-  subscription_id = "81e63c6d-7f3c-4e33-8a28-0036da979a43"
 }
